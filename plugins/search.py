9f8b8a6f961f466d4436e62c39a07374cf3ba36f
MasayoshiTsukioka <masayoshiand@gmail.com>
2019-12-14 16:18:33 +0900

    未完成search部分
    search部分おねがいします

--- plugins/search.py
+++ plugins/search.py
@@ -1,11 +1,27 @@
-from flask import render_template
-from flask_classy import FlaskView
-from model import Conversation
+from flask import render_template,Flask,request
+from flask_classy import FlaskView,route
+from model import Conversation,session
 from jinja2 import Environment, FileSystemLoader
 
+
+def DB_check(word,time):
+    for row in session.query(Conversation).filter(Conversation.text.like('%'+word+'%')).filter(Conversation.speaked_at == time):
+        print(row.text, row.speaked_at)
+        return row
+    return "該当ない"
 
 class SearchView(FlaskView):
     plugin_name = 'search'
 
-    def index(self):
-        return render_template('plugins/search/index.html', title="this is search title")
+    def home(self):
+        return render_template('plugins/search/home.html', title="this is search title")
+
+    @route('/result', methods=['GET','POST'])
+    def result(self):
+        #word = request.form['text']
+        #time = request.form['speaked_at']
+        row = DB_check()
+        return render_template('plugins/search/result.html',DB = "hello")
+
+    def w_search(self):
+        return render_template('plugins/search/search.html')
