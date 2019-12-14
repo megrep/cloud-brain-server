e1e7a2d1b294586d5560b1a75d95e91b44107fd8
akakou <akakou571@gmail.com>
2019-12-14 13:14:05 +0900

    テンプレート読み込みを完成

--- plugins/search.py
+++ plugins/search.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_classy import FlaskView
 from model import Conversation
 from jinja2 import Environment, FileSystemLoader
@@ -7,4 +8,4 @@ class SearchView(FlaskView):
     plugin_name = 'search'
 
     def index(self):
-        return "this is search page"
+        return render_template('plugins/search/index.html', title="this is search title")
